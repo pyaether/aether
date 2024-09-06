65621a258dfe5202285416b670718a7bfc8e024b
Saurabh Ghanekar <ghanekarsaurabh8@gmail.com>
2024-09-05 17:14:50 -0700

    Add function to merge html class_attribute

--- src/pytempl/__init__.py
+++ src/pytempl/__init__.py
@@ -1,6 +1,7 @@
-from pytempl import tags
-from pytempl.base import BaseWebElement
-from pytempl.safe_string import mark_safe
+from . import tags
+from .base import BaseWebElement
+from .safe_string import mark_safe
+from .utils import html_class_merge
 
 
 def render(root: BaseWebElement) -> str:
@@ -8,4 +9,4 @@ def render(root: BaseWebElement) -> str:
 
 
 __version__ = "0.0.0"
-__all__ = ["render", "tags", "BaseWebElement", "mark_safe"]
+__all__ = ["render", "tags", "BaseWebElement", "mark_safe", "html_class_merge"]
