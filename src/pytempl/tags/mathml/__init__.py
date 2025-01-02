056596fd73fa87add1e8188a0317c62bde925b14
Saurabh Ghanekar <ghanekarsaurabh8@gmail.com>
2025-01-02 13:37:21 -0500

    Expose MathAttributes

--- src/pytempl/tags/mathml/__init__.py
+++ src/pytempl/tags/mathml/__init__.py
@@ -1,10 +1,6 @@
 # https://developer.mozilla.org/en-US/docs/Web/MathML
 
 from ._base import BaseMathMLElement, GlobalMathMLAttributes
-from .math import Math
+from .math import Math, MathAttributes
 
-__all__ = [
-    "BaseMathMLElement",
-    "GlobalMathMLAttributes",
-    "Math",
-]
+__all__ = ["BaseMathMLElement", "GlobalMathMLAttributes", "Math", "MathAttributes"]
