6d34abfe06a1bb262c71cf207f844ab2cc819f93
Saurabh Ghanekar <ghanekarsaurabh8@gmail.com>
2025-04-20 16:01:31 -0700

    Update alpine.js plugin
    - Create AlpineJSData and Statement classes

--- src/pytempl/plugins/alpinejs/__init__.py
+++ src/pytempl/plugins/alpinejs/__init__.py
@@ -1,36 +1,91 @@
+from __future__ import (
+    annotations,
+)  # Remove this when Python 3.12 becomes the minimum supported version.
+
+from collections import UserString
 from collections.abc import Mapping, Sequence
-from typing import Any
+from typing import Any, Literal, TypeAlias
+
+# Remove this try/except block when Python 3.12 becomes the minimum supported version.
+try:
+    # Python 3.12+
+    _AlpineDataType: TypeAlias = (
+        bool
+        | str
+        | int
+        | float
+        | None
+        | "Statement"
+        | Sequence[Any]
+        | Mapping[str, Any]
+    )
+except TypeError:
+    from typing import Union
+
+    _AlpineDataType: TypeAlias = Union[
+        bool, str, int, float, None, "Statement", Sequence[Any], Mapping[str, Any]
+    ]
 
 
-def python_object_to_alpinejs_x_data(data: dict[str, Any]) -> str:
-    data_list = []
-    for key, value in data.items():
-        if isinstance(value, str):
-            data_list.append(f"{key}: '{value}'")
-        elif isinstance(value, bool):
-            data_list.append(f"{key}: {str(value).lower()}")
-        elif isinstance(value, int | float | Sequence):
-            data_list.append(f"{key}: {value}")
-        elif isinstance(value, Mapping):
-            data_list.append(f"{key}: {python_object_to_alpinejs_x_data(value)}")
-        else:
-            raise ValueError(f"Unsupported value type: {type(value)}")
+class Statement(UserString):
+    def __init__(
+        self, content: str, seq_type: Literal["assignment", "definition", "instance"]
+    ):
+        self.seq_type = seq_type
 
-    return f"{{{', '.join(data_list)}}}"
+        super().__init__(content)
+
+    def statement_type(self) -> str:
+        return self.seq_type
 
 
-def python_object_to_alpinejs_x_init(data: dict[str, Any]) -> str:
-    data_list = []
-    for key, value in data.items():
-        if isinstance(value, str):
-            data_list.append(f"{key} = '{value}'")
-        elif isinstance(value, bool):
-            data_list.append(f"{key} = {str(value).lower()}")
-        elif isinstance(value, int | float | Sequence):
-            data_list.append(f"{key} = {value}")
-        elif isinstance(value, Mapping):
-            data_list.append(f"{key} = {python_object_to_alpinejs_x_data(value)}")
-        else:
-            raise ValueError(f"Unsupported value type: {type(value)}")
+class AlpineJSData(str):
+    def __new__(
+        cls, data: _AlpineDataType, directive: Literal["x-data", "x-init"] = "x-data"
+    ):
+        parsed_data_object = cls._parse_object(data, directive)
+        return super().__new__(cls, parsed_data_object)
 
-    return f"{', '.join(data_list)}"
+    @classmethod
+    def _parse_object(
+        cls,
+        data: dict[str, _AlpineDataType],
+        directive: Literal["x-data", "x-init"],
+    ) -> str:
+        data_list = []
+        match directive:
+            case "x-data":
+                separation = ": "
+            case "x-init":
+                separation = " = "
+
+        for key, value in data.items():
+            match value:
+                case Statement():
+                    match value.seq_type:
+                        case "definition":
+                            data_list.append(f"{key} {value}")
+                        case "assignment":
+                            data_list.append(f"{key} = {value}")
+                        case "instance":
+                            data_list.append(f"{value}")
+                case bool():
+                    data_list.append(f"{key}{separation}{str(value).lower()}")
+                case str():
+                    data_list.append(f"{key}{separation}'{value}'")
+                case int() | float() | Sequence():
+                    data_list.append(f"{key}{separation}{value}")
+                case None:
+                    data_list.append(f"{key}{separation}null")
+                case Mapping():
+                    data_list.append(
+                        f"{key}{separation}{cls._parse_object(value, directive)}"
+                    )
+                case _:
+                    raise ValueError(f"Unsupported value type: {type(value)}")
+
+        match directive:
+            case "x-data":
+                return f"{{ {', '.join(data_list)} }}"
+            case "x-init":
+                return f"{', '.join(data_list)}"
